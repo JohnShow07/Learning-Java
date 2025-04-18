40e37670a2251b1cac649fc1c024ba1a8986240b
JohnShow07 <cevdeteken23it@gmail.com>
2025-04-18 11:00:02 +0200

    method gibListeMitEingefügtemWert

--- src/main/java/woche1.java
+++ src/main/java/woche1.java
@@ -3,15 +3,20 @@ import java.util.ArrayList;
 
 public class woche1 {
 
+    public static List<Integer> gibListeMitEingefügtemWert() {
+        List<Integer> ints = new ArrayList<>(List.of(1, 2, 3, 4, 5));
+        ints.add(2, 10);
+        return ints;
+    }
+
     public static void main(String[] args) {
         List<Integer> ints = new ArrayList<>(List.of(1, 2, 3, 4, 5));
 
-    
         ints.add(2, 10);
 
         System.out.println(ints.get(2));
-        
-        for(int i = 0; i < ints.size(); i++){
+
+        for (int i = 0; i < ints.size(); i++) {
             System.out.println(ints.get(i));
         }
 
