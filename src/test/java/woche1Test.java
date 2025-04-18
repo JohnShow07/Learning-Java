03c5252832827305c177b6d1318840b5f469b2b9
JohnShow07 <cevdeteken23it@gmail.com>
2025-04-18 19:44:50 +0200

    test was completed successfully

--- src/test/java/woche1Test.java
+++ src/test/java/woche1Test.java
@@ -1,3 +1,15 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
 public class woche1Test {
 
+    @Test
+    void testGibListeMitEingefügtemWert() {
+        List<Double> erwartet = List.of(1.2, 2.5, 10.0, 3.1, 4.0, 5.6);
+        List<Double> ergebnis = woche1.gibListeMitEingefügtemWert();
+        assertEquals(erwartet, ergebnis);
+
+    }
 }
