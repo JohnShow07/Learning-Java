a0b65158325eada9a84b6fa110954a868cf2df9d
JohnShow07 <cevdeteken23it@gmail.com>
2025-04-18 11:05:56 +0200

    List double instead of integer

--- src/main/java/woche1.java
+++ src/main/java/woche1.java
@@ -3,10 +3,10 @@ import java.util.ArrayList;
 
 public class woche1 {
 
-    public static List<Integer> gibListeMitEingefügtemWert() {
-        List<Integer> ints = new ArrayList<>(List.of(1, 2, 3, 4, 5));
-        ints.add(2, 10);
-        return ints;
+    public static List<Double> gibListeMitEingefügtemWert() {
+        List<Double> d = new ArrayList<>(List.of(1.2, 2.5, 3.1, 4.0, 5.6));
+        d.add(2, 10.0);
+        return d;
     }
 
     public static void main(String[] args) {
@@ -18,6 +18,12 @@ public class woche1 {
 
         for (int i = 0; i < ints.size(); i++) {
             System.out.println(ints.get(i));
+        }
+
+        List<Double> doubles = gibListeMitEingefügtemWert();
+
+        for (Double dd : doubles) {
+            System.out.println(dd);
         }
 
     }
--- src/test/java/woche1Test.java
+++ src/test/java/woche1Test.java
@@ -1,1 +1,3 @@
-cla
+public class woche1Test {
+
+}
