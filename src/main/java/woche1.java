da441c44142a12342ce4c76e33301c04ad6b356b
JohnShow07 <cevdeteken23it@gmail.com>
2025-04-19 07:39:08 +0200

    queue method was created

--- src/main/java/woche1.java
+++ src/main/java/woche1.java
@@ -1,5 +1,7 @@
 import java.util.List;
+import java.util.Queue;
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class woche1 {
 
@@ -7,6 +9,22 @@ public class woche1 {
         List<Double> d = new ArrayList<>(List.of(1.2, 2.5, 3.1, 4.0, 5.6));
         d.add(2, 10.0);
         return d;
+    }
+
+    public static Queue<String> queueMethod() {
+        Queue<String> q = new LinkedList<>();
+
+        q.offer("a");
+        q.offer("b");
+        q.offer("c");
+
+        System.out.println(q);
+        System.out.println(q.peek()); // Schau den ersten Element
+        System.out.println(q);
+        System.out.println(q.poll());// Nimm den ersten Element raus
+        System.out.println(q);
+
+        return q;
     }
 
     public static void main(String[] args) {
@@ -25,6 +43,9 @@ public class woche1 {
         for (Double dd : doubles) {
             System.out.println(dd);
         }
+
+        // Queue
+        queueMethod();
 
     }
 }
